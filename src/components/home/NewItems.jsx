5d0a11967665d304a0d9181408bb64eeca043440
Ishaq-H <itzizak@hotmail.com>
2023-08-28 20:54:45 +0100

    Animations

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -26,107 +26,125 @@ const NewItems = () => {
 
   return (
     <section id="section-items" className="no-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>New Items</h2>
-              <div className="small-border bg-color-2"></div>
+      <div
+        data-aos="fade-left"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h2>New Items</h2>
+                <div className="small-border bg-color-2"></div>
+              </div>
             </div>
-          </div>
-          {newNFTs.length > 0 && !loading ? (
-            <OwlCarousel
-              className="owl-theme"
-              items={4}
-              loop
-              margin={10}
-              nav
-              dots={false}
-              responsive={{
-                0: { items: 1 },
-                500: { items: 2 },
-                768: { items: 3 },
-                1000: { items: 4 },
-              }}
-            >
-              {newNFTs.map((newNFT, index) => (
+            {newNFTs.length > 0 && !loading ? (
+              <OwlCarousel
+                className="owl-theme"
+                items={4}
+                loop
+                margin={10}
+                nav
+                dots={false}
+                responsive={{
+                  0: { items: 1 },
+                  500: { items: 2 },
+                  768: { items: 3 },
+                  1000: { items: 4 },
+                }}
+              >
+                {newNFTs.map((newNFT, index) => (
+                  <div
+                    // className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                    key={index}
+                  >
+                    <div className="nft__item">
+                      <div className="author_list_pp">
+                        <Link
+                          to={`/author/${newNFT.authorId}`}
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="top"
+                          title="Creator: Monica Lucas"
+                        >
+                          <img
+                            className="lazy"
+                            src={newNFT.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <Countdown info={newNFT}></Countdown>
+                      <div className="nft__item_wrap">
+                        <div className="nft__item_extra">
+                          <div className="nft__item_buttons">
+                            <button>Buy Now</button>
+                            <div className="nft__item_share">
+                              <h4>Share</h4>
+                              <a href="" target="_blank" rel="noreferrer">
+                                <i className="fa fa-facebook fa-lg"></i>
+                              </a>
+                              <a href="" target="_blank" rel="noreferrer">
+                                <i className="fa fa-twitter fa-lg"></i>
+                              </a>
+                              <a href="">
+                                <i className="fa fa-envelope fa-lg"></i>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+
+                        <Link to={`/item-details/${newNFT.nftId}`}>
+                          <img
+                            src={newNFT.nftImage}
+                            className="lazy nft__item_preview"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft__item_info">
+                        <Link to={`/item-details/${newNFT.nftId}`}>
+                          <h4>{newNFT.title}</h4>
+                        </Link>
+                        <div className="nft__item_price">
+                          {newNFT.price} ETH
+                        </div>
+                        <div className="nft__item_like">
+                          <i className="fa fa-heart"></i>
+                          <span>{newNFT.likes}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </OwlCarousel>
+            ) : (
+              new Array(4).fill(0).map((_, index) => (
                 <div
-                  // className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                  className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
                   key={index}
                 >
                   <div className="nft__item">
                     <div className="author_list_pp">
-                      <Link
-                        to={`/author/${newNFT.authorId}`}
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        title="Creator: Monica Lucas"
-                      >
-                        <img className="lazy" src={newNFT.authorImage} alt="" />
-                        <i className="fa fa-check"></i>
-                      </Link>
+                      {/* <Skeleton width={60} height={60} borderRadius={9999} /> */}
                     </div>
-                    <Countdown info={newNFT}></Countdown>
                     <div className="nft__item_wrap">
-                      <div className="nft__item_extra">
-                        <div className="nft__item_buttons">
-                          <button>Buy Now</button>
-                          <div className="nft__item_share">
-                            <h4>Share</h4>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-facebook fa-lg"></i>
-                            </a>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-twitter fa-lg"></i>
-                            </a>
-                            <a href="">
-                              <i className="fa fa-envelope fa-lg"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-
-                      <Link to={`/item-details/${newNFT.nftId}`}>
-                        <img
-                          src={newNFT.nftImage}
-                          className="lazy nft__item_preview"
-                          alt=""
-                        />
-                      </Link>
+                      <Skeleton width="100%" height="100%" borderRadius={5} />
                     </div>
-                    <div className="nft__item_info">
-                      <Link to={`/item-details/${newNFT.nftId}`}>
-                        <h4>{newNFT.title}</h4>
-                      </Link>
-                      <div className="nft__item_price">{newNFT.price} ETH</div>
-                      <div className="nft__item_like">
-                        <i className="fa fa-heart"></i>
-                        <span>{newNFT.likes}</span>
+                    <div
+                      className="nft__item_info"
+                      style={{ marginTop: "15px" }}
+                    >
+                      <div className="nft__item_price">
+                        <Skeleton width="40%" height={20} borderRadius={5} />
                       </div>
                     </div>
                   </div>
                 </div>
-              ))}
-            </OwlCarousel>
-          ) : (
-            new Array(4).fill(0).map((_, index) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-                <div className="nft__item">
-                  <div className="author_list_pp">
-                    {/* <Skeleton width={60} height={60} borderRadius={9999} /> */}
-                  </div>
-                  <div className="nft__item_wrap">
-                    <Skeleton width="100%" height="100%" borderRadius={5} />
-                  </div>
-                  <div className="nft__item_info" style={{ marginTop: "15px" }}>
-                    <div className="nft__item_price">
-                      <Skeleton width="40%" height={20} borderRadius={5} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
+              ))
+            )}
+          </div>
         </div>
       </div>
     </section>
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -22,50 +22,56 @@ const TopSellers = () => {
 
   return (
     <section id="section-popular" className="pb-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Top Sellers</h2>
-              <div className="small-border bg-color-2"></div>
+      <div
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h2>Top Sellers</h2>
+                <div className="small-border bg-color-2"></div>
+              </div>
             </div>
-          </div>
-          <div className="col-md-12">
-            <ol className="author_list">
-              {sellers.length > 0 && !loading
-                ? sellers.map((seller, index) => (
-                    <li key={index}>
-                      <div className="author_list_pp">
-                        <Link to={`/author/${seller.authorId}`}>
-                          <img
-                            className="lazy pp-author"
-                            src={seller.authorImage}
-                            alt=""
+            <div className="col-md-12">
+              <ol className="author_list">
+                {sellers.length > 0 && !loading
+                  ? sellers.map((seller, index) => (
+                      <li key={index}>
+                        <div className="author_list_pp">
+                          <Link to={`/author/${seller.authorId}`}>
+                            <img
+                              className="lazy pp-author"
+                              src={seller.authorImage}
+                              alt=""
+                            />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <div className="author_list_info">
+                          <Link to={`/author/${seller.authorId}`}>
+                            {seller.authorName}
+                          </Link>
+                          <span>{seller.price}</span>
+                        </div>
+                      </li>
+                    ))
+                  : new Array(12).fill(0).map((_, index) => (
+                      <li key={index}>
+                        <div className="author_list_pp">
+                          <Skeleton
+                            width="50px"
+                            height="50px"
+                            borderRadius={999}
                           />
-                          <i className="fa fa-check"></i>
-                        </Link>
-                      </div>
-                      <div className="author_list_info">
-                        <Link to={`/author/${seller.authorId}`}>
-                          {seller.authorName}
-                        </Link>
-                        <span>{seller.price}</span>
-                      </div>
-                    </li>
-                  ))
-                : new Array(12).fill(0).map((_, index) => (
-                    <li key={index}>
-                      <div className="author_list_pp">
-                        <Skeleton
-                          width="50px"
-                          height="50px"
-                          borderRadius={999}
-                        />
-                      </div>
-                      <div className="author_list_info"></div>
-                    </li>
-                  ))}
-            </ol>
+                        </div>
+                        <div className="author_list_info"></div>
+                      </li>
+                    ))}
+              </ol>
+            </div>
           </div>
         </div>
       </div>
